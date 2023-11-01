03ebb3df15726af33ccae1a099adba1fba728fc7
jtdimasaka <jtd33@cam.ac.uk>
2023-11-01 22:26:11 +0000

    Update sql module

--- notebooks/2.0-data-download.sql
+++ notebooks/2.0-data-download.sql
@@ -1,1 +1,25 @@
-SELECT * FROM duckdb_extensions();
+-- INSTALL spatial;
+-- INSTALL httpfs;
+
+LOAD spatial;
+LOAD httpfs;
+SET s3_region='us-west-2';
+
+COPY (
+    SELECT
+        type,
+        version,
+        CAST(updatetime as varchar) as updateTime,
+        height,
+        numfloors as numFloors,
+        level,
+        class,
+        JSON(names) as names,
+        JSON(sources) as sources,
+        ST_GeomFromWKB(geometry) as geometry
+    FROM
+        read_parquet('s3://overturemaps-us-west-2/release/2023-10-19-alpha.0/theme=buildings/type=*/*', hive_partitioning=1)
+    LIMIT
+        100
+    ) TO 'buildings_sample.geojsonseq'
+WITH (FORMAT GDAL, DRIVER 'GeoJSONSeq');
