fc42bede071172fbf82fccee66113ba60a94632a
jtdimasaka <jtd33@cam.ac.uk>
2023-12-31 14:43:09 +0800

    Created generate_tile.m module to tile ground truth labels

--- notebooks/matlab/convert_attr_into_rasters.m
+++ notebooks/matlab/convert_attr_into_rasters.m
@@ -24,39 +24,41 @@ for i = 1:numel(country_path)
     csv_path = path_i + string(csv_file);
     labels = readtable(csv_path);
     bldgtype = unique(labels.OrgConstructionCode);
+
+    % make new directory for rasterized whole map of attributes
+    [~,~] = mmkdir(path_i+"attr_rasterized");
     
-        % select building type 'j'
-        for j = 1:numel(bldgtype)
+    % select building type 'j'
+    for j = 1:numel(bldgtype)
+    
+        % get indices to assign values
+        loc_j = labels.LocGroup(string(labels.OrgConstructionCode)==string(bldgtype{j,1}));
+        [~,Locb] = ismember(loc_j,loc(:)); %Locb contains the 'loc' indices
+        [~,Loc_labels] = ismember( ...
+            table(loc_j, repelem(string(bldgtype{j,1}),numel(Locb))', ...
+            'VariableNames',{'Var1','Var2'}),...
+            table(labels.LocGroup, string(labels.OrgConstructionCode), ...
+            'VariableNames',{'Var1','Var2'}) );
         
-            % get indices to assign values
-            loc_j = labels.LocGroup(string(labels.OrgConstructionCode)==string(bldgtype{j,1}));
-            [~,Locb] = ismember(loc_j,loc(:)); %Locb contains the 'loc' indices
-            [~,Loc_labels] = ismember( ...
-                table(loc_j, repelem(string(bldgtype{j,1}),numel(Locb))', ...
-                'VariableNames',{'Var1','Var2'}),...
-                table(labels.LocGroup, string(labels.OrgConstructionCode), ...
-                'VariableNames',{'Var1','Var2'}) );
-            
-            % create arrays
-            map_farea = 0.*loc;
-            map_farea(Locb) = labels.FloorArea(Loc_labels);
-            map_nbldg = 0.*loc;
-            map_nbldg(Locb) = labels.NumberOfBuildings(Loc_labels);
-            map_bldgtiv = 0.*loc;
-            map_bldgtiv(Locb) = labels.BuildingTIV(Loc_labels);
-            
-            % save rasters
-            mkdir(path_i+"attr_rasterized")
-            geotiffwrite(path_i+"attr_rasterized"+"/"+ ...
-                loc_raster_file(1:3)+"_farea_"+string(bldgtype{j,1})+".tif", ...
-                map_farea,locR)
-            geotiffwrite(path_i+"attr_rasterized"+"/"+ ...
-                loc_raster_file(1:3)+"_nbldg_"+string(bldgtype{j,1})+".tif", ...
-                map_nbldg,locR)
-            geotiffwrite(path_i+"attr_rasterized"+"/"+ ...
-                loc_raster_file(1:3)+"_bldgtiv_"+string(bldgtype{j,1})+".tif", ...
-                map_bldgtiv,locR)
-        end
+        % create arrays
+        map_farea = 0.*loc;
+        map_farea(Locb) = labels.FloorArea(Loc_labels);
+        map_nbldg = 0.*loc;
+        map_nbldg(Locb) = labels.NumberOfBuildings(Loc_labels);
+        map_bldgtiv = 0.*loc;
+        map_bldgtiv(Locb) = labels.BuildingTIV(Loc_labels);
+        
+        % save rasters
+        geotiffwrite(path_i+"attr_rasterized"+"/"+ ...
+            loc_raster_file(1:3)+"_farea_"+string(bldgtype{j,1})+".tif", ...
+            map_farea,locR)
+        geotiffwrite(path_i+"attr_rasterized"+"/"+ ...
+            loc_raster_file(1:3)+"_nbldg_"+string(bldgtype{j,1})+".tif", ...
+            map_nbldg,locR)
+        geotiffwrite(path_i+"attr_rasterized"+"/"+ ...
+            loc_raster_file(1:3)+"_bldgtiv_"+string(bldgtype{j,1})+".tif", ...
+            map_bldgtiv,locR)
+    end
 
     toc
 end
